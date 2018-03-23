e58efeece3090b60608c84d79f96e9e81e511092
danilchican <danilchican@mail.ru>
2018-03-23 13:54:15 +0300

    Created layout for dashboard

--- config/app.php
+++ config/app.php
@@ -14,6 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'TokenFund'),
+    'dashboard' => env('DASHBOARD_NAME', 'Dashboard'),
 
     /*
     |--------------------------------------------------------------------------
