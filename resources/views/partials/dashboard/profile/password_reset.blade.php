bc76b0c89bd733a0dba4dcffa1591eb371c2ddff
danilchican <danilchican@mail.ru>
2018-03-25 11:42:27 +0300

    Modified reset form and added js validation

--- resources/views/partials/dashboard/profile/password_reset.blade.php
+++ resources/views/partials/dashboard/profile/password_reset.blade.php
@@ -1,21 +1,41 @@
-<form id="profile-password-reset" data-parsley-validate class="form-horizontal form-label-left">
-    <div class="form-group">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12">@lang('dashboard.auth.profile.old_password') <span class="required">*</span></label>
-        <div class="col-md-9 col-sm-9 col-xs-12">
-            <input type="old-password" class="form-control" required="required">
-        </div>
-    </div>
-    <div class="form-group">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12">@lang('dashboard.auth.profile.password') <span class="required">*</span></label>
-        <div class="col-md-9 col-sm-9 col-xs-12">
-            <input type="password" class="form-control" required="required">
-        </div>
-    </div>
-    <div class="form-group">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12">@lang('dashboard.auth.profile.password_confirmation') <span
+<form action="{{ route('dashboard.profile.password.reset') }}" method="POST" id="profile-password-reset"
+      data-parsley-validate class="form-horizontal form-label-left">
+    {{ csrf_field() }}
+
+    <div class="form-group{{ $errors->has('old_password') ? ' has-error' : '' }}">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12">@lang('dashboard.auth.profile.old_password') <span
                     class="required">*</span></label>
         <div class="col-md-9 col-sm-9 col-xs-12">
-            <input type="password" name="password-confirmation" class="form-control" required="required">
+            <input type="password" name="old_password" class="form-control" minlength="6" required="required">
+            @if ($errors->has('old_password'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('old_password') }}</strong>
+                </span>
+            @endif
+        </div>
+    </div>
+    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12">@lang('dashboard.auth.profile.password') <span
+                    class="required">*</span></label>
+        <div class="col-md-9 col-sm-9 col-xs-12">
+            <input type="password" id="password" name="password" class="form-control" minlength="6" data-parsley-equalto="#password_confirmation" required="required">
+            @if ($errors->has('password'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+            @endif
+        </div>
+    </div>
+    <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12">@lang('dashboard.auth.profile.password_confirmation')
+            <span class="required">*</span></label>
+        <div class="col-md-9 col-sm-9 col-xs-12">
+            <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" minlength="6" data-parsley-equalto="#password" required="required">
+            @if ($errors->has('password_confirmation'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('password_confirmation') }}</strong>
+                </span>
+            @endif
         </div>
     </div>
     <div class="ln_solid"></div>
