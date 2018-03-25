37a17f5435d48b42bdfab171448c315789242a22
danilchican <danilchican@mail.ru>
2018-03-25 10:11:49 +0300

    Created base form to reset password

---
+++ resources/views/partials/dashboard/profile/password_reset.blade.php
@@ -0,0 +1,21 @@
+<form id="profile-password-reset" data-parsley-validate class="form-horizontal form-label-left">
+    <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12">Password <span class="required">*</span></label>
+        <div class="col-md-9 col-sm-9 col-xs-12">
+            <input type="password" class="form-control" required="required">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12">Password Confirmation <span
+                    class="required">*</span></label>
+        <div class="col-md-9 col-sm-9 col-xs-12">
+            <input type="password" name="password-confirmation" class="form-control" required="required">
+        </div>
+    </div>
+    <div class="ln_solid"></div>
+    <div class="form-group">
+        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+            <button type="submit" class="btn btn-success">Submit</button>
+        </div>
+    </div>
+</form>
