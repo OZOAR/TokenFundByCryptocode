d64a2a619afb8c9ffca48b9693f51a645a4f43a5
danilchican <danilchican@mail.ru>
2018-04-14 22:56:11 +0300

    Added controller

--- routes/web.php
+++ routes/web.php
@@ -72,3 +72,8 @@ Route::group(['prefix' => '/dashboard', 'middleware' => ['admin']], function () 
 Route::group(['prefix' => '/profile', 'middleware' => ['auth']], function () {
     $this->get('/', 'ClientProfileController@showProfile')->name('profile.show');
 });
+
+Route::group(['prefix' => '/statistics'], function () {
+    $this->get('/graph/main', 'StatisticsController@getMainGraph');
+});
+
